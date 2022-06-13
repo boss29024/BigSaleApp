580d94b3f2d11125c6bc641398f763f7020ce6a6
boss29024 <boss29024@gmail.com>
2022-06-13 13:31:15 +0700

    initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
     );
   }
 }
-
